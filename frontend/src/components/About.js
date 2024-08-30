91fc9b8b658d1174b51b56c3b8a3da95062836cf
NicolasLepki <nicolaslepki@gmail.com>
2024-08-29 23:55:49 -0400

    added a template About.js

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,1 +1,14 @@
 // about page component
+
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About This Project</h1>
+            <p>This is a digit recognition project using a neural network trained on the MNIST dataset. etc etc etc add info here....</p>
+        </div>
+    );
+};
+
+export default About;
