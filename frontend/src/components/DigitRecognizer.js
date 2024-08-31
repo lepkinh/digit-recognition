ba005ef9eee1e022f4eef42cdb29a00680222871
NicolasLepki <nicolaslepki@gmail.com>
2024-08-31 01:32:33 -0400

    comment

--- frontend/src/components/DigitRecognizer.js
+++ frontend/src/components/DigitRecognizer.js
@@ -28,7 +28,7 @@ const DigitRecognizer = () => {
     <div className="flex flex-col items-center">
       <canvas
         ref={canvasRef}
-        width={280}
+        width={280} // Change this to 28x28
         height={280}
         className="border border-gray-300"
       />
